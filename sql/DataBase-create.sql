01c7769687684f5c72d3ee304f46739fb9cd2e2c
Gerk <vgerk95@gmail.com>
2019-05-29 21:40:55 +0300

    database-sql-files-fix

--- sql/DataBase-create.sql
+++ sql/DataBase-create.sql
@@ -1,24 +1,29 @@
-CREATE TABLE guide (
+CREATE TABLE guide
+(
   guide_id   BIGSERIAL PRIMARY KEY,
   username   TEXT NOT NULL UNIQUE,
   password   TEXT NOT NULL,
   fio        TEXT,
-  age        SMALLINT CONSTRAINT adult_only CHECK (age > 18),
+  age        SMALLINT
+    CONSTRAINT adult_only CHECK (age > 18),
   experience SMALLINT,
   languages  TEXT
 );
 
-CREATE TABLE tour (
+CREATE TABLE tour
+(
   tour_id          BIGSERIAL PRIMARY KEY,
   theme            TEXT,
   type_of_exhibits TEXT,
   duration         SMALLINT,
-  cost             DOUBLE PRECISION CONSTRAINT positive_price CHECK (cost > 0),
+  cost             DOUBLE PRECISION
+    CONSTRAINT positive_price CHECK (cost > 0),
   image_url        TEXT,
   guide_id         BIGINT REFERENCES guide (guide_id)
 );
 
-CREATE TABLE exhibit (
+CREATE TABLE exhibit
+(
   exhibit_id  BIGSERIAL PRIMARY KEY,
   title       TEXT,
   dated       TEXT,
@@ -28,40 +33,47 @@ CREATE TABLE exhibit (
   image_url   TEXT
 );
 
-CREATE TABLE tour_exhibit (
+CREATE TABLE tour_exhibit
+(
   tour_exhibit_id BIGSERIAL PRIMARY KEY,
   tour_id         BIGINT REFERENCES tour (tour_id),
   exhibit_id      BIGINT REFERENCES exhibit (exhibit_id)
 );
 
-CREATE TABLE visitor (
+CREATE TABLE visitor
+(
   visitor_id BIGSERIAL PRIMARY KEY,
   username   TEXT NOT NULL UNIQUE,
   password   TEXT NOT NULL,
   fio        TEXT,
-  age        BIGINT CONSTRAINT adult_only CHECK (age > 0),
+  age        BIGINT
+    CONSTRAINT adult_only CHECK (age > 0),
   email      TEXT
 );
 
-CREATE TABLE tour_visitor (
+CREATE TABLE tour_visitor
+(
   tour_visitor_id BIGSERIAL PRIMARY KEY,
-  tour_id    BIGINT REFERENCES tour (tour_id),
-  visitor_id BIGINT REFERENCES visitor (visitor_id)
+  tour_id         BIGINT REFERENCES tour (tour_id),
+  visitor_id      BIGINT REFERENCES visitor (visitor_id)
 );
 
 
-CREATE TABLE users (
+CREATE TABLE users
+(
   id       BIGSERIAL PRIMARY KEY,
   username TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE roles
+(
   id   BIGSERIAL PRIMARY KEY,
   name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE users_roles (
+CREATE TABLE users_roles
+(
   id      BIGSERIAL PRIMARY KEY,
   user_id BIGINT REFERENCES users (id),
   role_id BIGINT REFERENCES roles (id),
--- sql/DataBase-delete.sql
+++ sql/DataBase-delete.sql
@@ -1,2 +1,1 @@
-
 DROP TABLE exhibit, guide, tour_exhibit, tour_visitor, tour, visitor, users, roles, users_roles;
