7c8f1dd75d1efbb9a2e1cef45b42f4b6d0bf00e8
heorhi.vovk <heorhi.vovk@gmail.com>
2019-06-05 12:10:56 +0300

    web-socket-chat-session-add

--- src/main/java/by/home/museum/controller/WebSocketController.java
+++ src/main/java/by/home/museum/controller/WebSocketController.java
@@ -1,7 +1,12 @@
 package by.home.museum.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
@@ -23,23 +28,30 @@ public class WebSocketController {
     }
 
     @MessageMapping(RECEIVING_URL)
-    public void onReceivedMessage(String message) {
-        System.out.println("New message received : " + message);
-        sendMessage();
+    public void onReceivedMessage(@Header("simpSessionId") String sessionId, @Payload String message) {
+        String processedMessage = message.replace("\"", "");
+        if (processedMessage.equals("test")) {
+            counter.set(0L);
+        }
+        System.out.println("New message received : " + processedMessage + ", from session - " + sessionId);
+        template.convertAndSendToUser(sessionId, SENDING_URL, buildNextMessage(), createHeaders(sessionId));
     }
 
-    @SubscribeMapping(SENDING_URL)
-    public String onSubscribe() {
+    @SubscribeMapping("/user" + SENDING_URL)
+    public String onSubscribe(@Header("simpSessionId") String sessionId) {
         return "Backend carefully listens to you.";
-    }
-
-    private void sendMessage() {
-        template.convertAndSend(SENDING_URL, buildNextMessage());
     }
 
     private String buildNextMessage() {
         String message = "Message from back - " + counter.getAndIncrement();
         System.out.println("Response to front - " + message);
         return message;
+    }
+
+    private MessageHeaders createHeaders(String sessionId) {
+        SimpMessageHeaderAccessor headerAccessor = SimpMessageHeaderAccessor.create(SimpMessageType.MESSAGE);
+        headerAccessor.setSessionId(sessionId);
+        headerAccessor.setLeaveMutable(true);
+        return headerAccessor.getMessageHeaders();
     }
 }
